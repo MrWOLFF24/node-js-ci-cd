dc80c422fd24fc0ff95ac417f8a1db3c080c08d5
mac <mac@mac.local>
2018-10-04 20:52:52 +0200

    simple nodejs app with test

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # node-js-ci-cd
-a simple project to test ci/cd automation code delivery with Jenkins and docker   
+
+a simple project to test ci/cd automation code delivery with Jenkins and docker
+
+feel free to use this project
---
+++ test/test.js
@@ -0,0 +1,8 @@
+const request = require('supertest');
+const app = require('../server.js');
+describe('GET / ', () => {
+  it('should respond with hello world', function (done) {
+    //navigate to root and check the the response is "hello world"
+    request(app).get('/').expect('hello world', done);
+  });
+});
